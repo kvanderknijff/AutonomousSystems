416304c6f8549d65928c121de86af88792515a71
Hidde <hidde.ger@icloud.com>
2026-05-27 10:56:39 +0200

    Chariot code toegevoegd

---
+++ Software/Chariot/Code/credentials.py
@@ -0,0 +1,18 @@
+'''
+insert here your network parameters creating two variables named ssid and pwd
+e.g.
+ssid='myssid'
+pwd='mypwd'
+
+register your device with its mac
+use 'network_test.py' to extract your own mac address
+e.g. MAC: 2C:CF:67:C1:8D:3F
+
+'''
+# You can always create a hotspot with your own parameters....
+#ssid=b'raven'
+#pwd=b'raven000'
+
+# or, if you registered your device from pulsar.
+ssid=b'iotroam'
+pwd=b'password provided by pulsar or set by you in registration'
