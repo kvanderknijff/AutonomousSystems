48c11c49e25f8ff359183ff7d5e5dd669558ee32
Draqy <peterh9792@gmail.com>
2026-06-06 10:19:23 +0200

    Update ArUcoDetection.py

--- Software/Detection/ArUcoDetection.py
+++ Software/Detection/ArUcoDetection.py
@@ -1,15 +1,56 @@
 import cv2
 import math
 import numpy as np
+import matplotlib.pyplot as plt
 
 dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
 detector = cv2.aruco.ArucoDetector(dictionary)
+
+def arUcoDetection(frame: np.ndarray):
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    corners, markerIDs, rejected = detector.detectMarkers(frame)
+    
+    information = []
+
+    if markerIDs is not None:
+        for corner, markerID in zip(corners, markerIDs):
+            corner = corner.reshape(4, 2)
+            corner = corner.astype(int)
+
+            topLeft, topRight, bottomRight, bottomLeft = corner
+
+            cv2.putText(frame, f"id: {markerID[0]}", tuple(topLeft), cv2.FONT_HERSHEY_PLAIN, 1.3, (255, 0, 255), 2)
+
+            centerX = (topLeft[0] + bottomRight[0]) // 2
+            centerY = (topLeft[1] + bottomRight[1]) // 2
+
+            center = (centerX, centerY)
+            cv2.circle(frame, center, 5, (0, 0, 255), -1)
+
+            topMiddleX = (topLeft[0] + topRight[0]) // 2
+            topMiddleY = (topLeft[1] + topRight[1]) // 2
+            
+            direction = math.degrees(math.atan2(topMiddleY - centerY, topMiddleX - centerX))
+            cv2.putText(frame, f"dir: {direction}", tuple(topRight), cv2.FONT_HERSHEY_PLAIN, 1.3, (255, 0, 255), 2)            
+            information.append([int(markerID[0]), center, direction])
+            
+    return information, frame
+
+def LEDDetection(frame: np.ndarray):
+    frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    r, g, b = cv2.split(frameRGB)
+    b_g = cv2.subtract(b,g)
+    r_b = cv2.subtract(r,b)
+    
+    
 
 def videoProcessing(file: str, record: bool, camera: bool):
     capture = cv2.VideoCapture(file)
 
     if not capture.isOpened():
-        print("Error: Could not open video.")
+        print("Error: Could not open video")
         return
 
     if record:
@@ -35,11 +76,12 @@ def videoProcessing(file: str, record: bool, camera: bool):
             if camera:
                 frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
                 
-            arUcoInformation, frame = arUcoDetection(frame)
-            cv2.imshow("Video", frame)
+            arUcoInformation, arUcoFrame = arUcoDetection(frame)
+            # LEDDetection(frame)
 
+            cv2.imshow("ArUcos", arUcoFrame)
             if record:
-                out.write(frame)
+                out.write(arUcoFrame)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
@@ -48,43 +90,9 @@ def videoProcessing(file: str, record: bool, camera: bool):
             print("Program ran into an exception: ", e)
 
     capture.release()
-    
     if record:
         out.release()
-
     cv2.destroyAllWindows()
-    
-def arUcoDetection(frame: np.ndarray):
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    corners, markerIDs, rejected = detector.detectMarkers(frame)
-    
-    information = []
-
-    if markerIDs is not None:
-        for corner, markerID in zip(corners, markerIDs):
-            corner = corner.reshape(4, 2)
-            corner = corner.astype(int)
-
-            topLeft, topRight, bottomRight, bottomLeft = corner
-
-            centerX = (topLeft[0] + bottomRight[0]) // 2
-            centerY = (topLeft[1] + bottomRight[1]) // 2
-
-            center = (centerX, centerY)
-            cv2.circle(frame, center, 5, (0, 0, 255), -1)
-
-            topMiddleX = (topLeft[0] + topRight[0]) // 2
-            topMiddleY = (topLeft[1] + topRight[1]) // 2
-            
-            direction = math.degrees(math.atan2(topMiddleY - centerY, topMiddleX - centerX))
-            cv2.putText(frame, f"dir: {direction}", tuple(topRight), cv2.FONT_HERSHEY_PLAIN, 1.3, (255, 0, 255), 2)
-            
-            cv2.putText(frame, f"id: {markerID[0]}", tuple(topLeft), cv2.FONT_HERSHEY_PLAIN, 1.3, (255, 0, 255), 2)
-            
-            information.append([int(markerID[0]), center, direction])
-            
-    return information, frame
 
 if __name__ == "__main__":
     #videoProcessing("http://145.137.57.80:8080/video", record=True, camera=True)
