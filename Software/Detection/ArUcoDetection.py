5f7d414852f011c60f918c3710299a713b26d968
Draqy <peterh9792@gmail.com>
2026-06-06 12:37:46 +0200

    first implimentation of led detection

--- Software/Detection/ArUcoDetection.py
+++ Software/Detection/ArUcoDetection.py
@@ -1,7 +1,6 @@
 import cv2
 import math
 import numpy as np
-import matplotlib.pyplot as plt
 
 dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
 detector = cv2.aruco.ArucoDetector(dictionary)
@@ -34,17 +33,68 @@ def arUcoDetection(frame: np.ndarray):
             direction = math.degrees(math.atan2(topMiddleY - centerY, topMiddleX - centerX))
             cv2.putText(frame, f"dir: {direction}", tuple(topRight), cv2.FONT_HERSHEY_PLAIN, 1.3, (255, 0, 255), 2)            
             information.append([int(markerID[0]), center, direction])
-            
+
     return information, frame
 
-def LEDDetection(frame: np.ndarray):
-    frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+def detect_pix (frame: np.ndarray, frameRGB: np.ndarray, color: str, method: int):
+    """
+    Function's base made by: Soufiane Lemkaddem, Hogeschool Rotterdam
+    """
+    ledPositions = []
+    Contours, hierarchy = cv2.findContours(frame, method, cv2.CHAIN_APPROX_SIMPLE)
+    for contour in Contours:
+        area = cv2.contourArea(contour)
+        if area > 500:
+            x, y, w, h = cv2.boundingRect(contour)
+            ledPositions.append((x + 0.5 * w, y + 0.5* h))
+            cv2.rectangle(frameRGB, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            print(color, np.round(x+(w/2)),np.round(y+(h/2)))
 
-    r, g, b = cv2.split(frameRGB)
-    b_g = cv2.subtract(b,g)
-    r_b = cv2.subtract(r,b)
+    if not ledPositions:
+        ledPositions.append((-1,-1))
+
+    return ledPositions, frameRGB
+
+def ledDetection(frameBGR: np.ndarray):
+    """
+    Function's base made by: Soufiane Lemkaddem, Hogeschool Rotterdam
+    """
     
-    
+    ledPositions = []
+
+    frameRGB = cv2.cvtColor(frameBGR, cv2.COLOR_BGR2RGB)
+    frameHSV = cv2.cvtColor(frameBGR, cv2.COLOR_BGR2HSV)
+
+    frameR, frameG, frameB = cv2.split(frameRGB)
+
+    frameBG = cv2.subtract(frameB, frameG)
+    frameRB = cv2.subtract(frameR, frameB)
+
+    # BLUE
+    ret, frameBG = cv2.threshold(frameBG, 60, 255, cv2.THRESH_BINARY)
+    blueLedPositions, blueLedframeRGB = detect_pix(frameBG, frameRGB, "Blue", cv2.RETR_TREE)
+    ledPositions.append(blueLedPositions)
+
+    # RED
+    ret, frameRB = cv2.threshold(frameRB, 60, 255, cv2.THRESH_BINARY)
+    redLedPositions, frameRGB = detect_pix(frameRB, frameRGB, "Red", cv2.RETR_TREE)
+    ledPositions.append(redLedPositions)
+
+    # GREEN
+    lowerGreen = np.array([40, 50, 50])
+    upperGreen = np.array([85, 255, 255])
+    maskG = cv2.inRange(frameHSV, lowerGreen, upperGreen)
+    greenLedPositions, frameRGB = detect_pix(maskG, frameRGB, "Green", cv2.RETR_EXTERNAL)
+    ledPositions.append(greenLedPositions)
+
+    # WHITE
+    lowerWhite = np.array([0, 0, 200])
+    upperWhite = np.array([180, 120, 255])
+    maskW = cv2.inRange(frameHSV, lowerWhite, upperWhite)
+    whiteLedPositions, frameRGB = detect_pix(maskW, frameRGB, "White", cv2.RETR_EXTERNAL)
+    ledPositions.append(whiteLedPositions)
+
+    return ledPositions, frameRGB
 
 def videoProcessing(file: str, record: bool, camera: bool):
     capture = cv2.VideoCapture(file)
@@ -63,7 +113,7 @@ def videoProcessing(file: str, record: bool, camera: bool):
         fps = capture.get(cv2.CAP_PROP_FPS)
 
         fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        out = cv2.VideoWriter('output.mp4', fourcc, fps, (width, height), isColor=False)
+        out = cv2.VideoWriter('output.mp4', fourcc, fps, (width, height)) #, isColor=False)
 
     while True:
         try:
@@ -72,20 +122,31 @@ def videoProcessing(file: str, record: bool, camera: bool):
             if not ret:
                 print("End of video")
                 break
-            
+
             if camera:
                 frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
-                
-            arUcoInformation, arUcoFrame = arUcoDetection(frame)
-            # LEDDetection(frame)
 
-            cv2.imshow("ArUcos", arUcoFrame)
+            arUcoInformation, arUcoFrame = arUcoDetection(frame)
+            """
+            arUcoInformation =
+            [[id1, center1, dir1],[id2,center2,dir2]]
+            """
+            ledPositions, frameRGB = ledDetection(frame)
+            """
+            ledPositions =
+            [[[blueled1x,blueled1y],[blueled2x,blueled2y]]
+            [[redled1x,redled1y],[redled2x,redled2y]]
+            [[greenled1x,greenled1y],[greenled2x,greenled2y]]
+            [[whiteled1x,whiteled1y],[whiteled2x,whiteled2y]]]
+            """
+
+            cv2.imshow("frame", frame)
             if record:
-                out.write(arUcoFrame)
+                out.write(frame)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
-        
+
         except Exception as e:
             print("Program ran into an exception: ", e)
 
@@ -95,5 +156,6 @@ def videoProcessing(file: str, record: bool, camera: bool):
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    #videoProcessing("http://145.137.57.80:8080/video", record=True, camera=True)
-    videoProcessing("C:/Vakken TI/Jaar 3/TINLAB - Autonomous Systems/Object detection AS/aruco test/arucoturntest.mp4", record=False, camera=False)
+    #videoProcessing("http://192.168.1.108:8080/video", record=True, camera=True)
+    #videoProcessing("C:/Vakken TI/Jaar 3/TINLAB - Autonomous Systems/Object detection AS/aruco test/arucoturntest.mp4", record=False, camera=False)
+    videoProcessing("C:/Vakken TI/Jaar 3/TINLAB - Autonomous Systems/Object detection AS/leds test/led_lightson_openwindow.mp4", record=True, camera=False)
